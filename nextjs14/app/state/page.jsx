96ae0f91b06e7568666e6b6bb181aca780446f1a
Malik-444 <talhahusnain475@gmail.com>
2024-01-04 21:28:46 +0500

    Tailwind,Daisy,Typography add

--- nextjs14/app/state/page.jsx
+++ nextjs14/app/state/page.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const page = () => {
-  return <div className="text-center text-2xl">State</div>;
+  const [count, setCount] = useState(0);
+
+  const AddOne = () => {
+    setCount(count + 1);
+  };
+  const SubOne = () => {
+    setCount(count - 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+  return (
+    <div className=" flex flex-col gap-y-3 justify-between items-center mt-6">
+      <div className="text-center text-2xl">you Clicked {count} time </div>
+      <div className="flex gap-x-5">
+        <button
+          onClick={AddOne}
+          className="text-3xl font-semibold text-red-400"
+        >
+          +
+        </button>
+        <button
+          onClick={SubOne}
+          className="text-3xl font-semibold text-red-400"
+        >
+          -
+        </button>
+      </div>
+      <button
+        onClick={handleReset}
+        className="text-xl py-1 px-2 rounded-2xl bg-slate-600 transition ease-in-out delay-150 text-white hover:text-black hover:bg-transparent"
+      >
+        Reset
+      </button>
+    </div>
+  );
 };
 
 export default page;
