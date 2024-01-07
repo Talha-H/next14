3ede62638430c8629a628d29e0391139c1cda90b
Malik-444 <talhahusnain475@gmail.com>
2024-01-07 23:17:14 +0500

    Add Prisma

--- nextjs14/app/prismaEx/page.jsx
+++ nextjs14/app/prismaEx/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import prisma from "@/utils/db";
 
-const page = () => {
+const prismaHandler = async () => {
+  await prisma.task.create({
+    data: {
+      content: "Hello Developers",
+    },
+  });
+  const allTask = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return allTask;
+};
+
+const page = async () => {
+  const tasks = await prismaHandler();
   return (
-    <div className='text-center'>Prisma</div>
-  )
-}
+    <div>
+      <h2 className="text-7xl">Prisma</h2>
+      {tasks.map((task) => {
+        return (
+          <h1 key={task.id} className="text-2xl py-2">
+            {task.content}
+          </h1>
+        );
+      })}
+    </div>
+  );
+};
 
-export default page
+export default page;
