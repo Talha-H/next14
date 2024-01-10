66b83b21e70a734f2ca385eedf9ba997af34eedc
Malik-444 <talhahusnain475@gmail.com>
2024-01-10 22:49:30 +0500

    prisma page

--- nextjs14/app/prismaEx/page.jsx
+++ nextjs14/app/prismaEx/page.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import prisma from "@/utils/db";
 
 const prismaHandler = async () => {
-  await prisma.task.create({
-    data: {
-      content: "Hello Developers",
-    },
-  });
+  // await prisma.task.create({
+  //   data: {
+  //     content: "Hello Developers",
+  //   },
+  // });
   const allTask = await prisma.task.findMany({
     orderBy: {
       createdAt: "desc",
@@ -17,6 +17,10 @@ const prismaHandler = async () => {
 
 const page = async () => {
   const tasks = await prismaHandler();
+
+  if (tasks.length == 0) {
+    return <h2 className="text-lg font-medium mt-8">No Task to show...</h2>;
+  }
   return (
     <div>
       <h2 className="text-7xl">Prisma</h2>
